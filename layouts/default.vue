533b8436efe36db07a3a06cce7ec338755a1997a
SazidAhmed <sazidahmed.official@gmail.com>
2021-11-16 21:59:30 +0600

    pages added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,90 +1,45 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
       fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      <NuxtLink to="/" class="mr-2">
+        Logo
+      </NuxtLink>
+      <v-spacer></v-spacer>
+      <NuxtLink to="/" class="ma-5">
+        <span class="grey--text">Home</span>
+      </NuxtLink>
+      <NuxtLink to="/about" class="ma-5">
+        <span class="grey--text">About</span>
+      </NuxtLink>
+      <NuxtLink to="/portfolio" class="ma-5">
+        <span class="grey--text">Portfolio</span>
+      </NuxtLink>
+      <NuxtLink to="/team" class="ma-5">
+        <span class="grey--text">Team</span>
+      </NuxtLink>
+      <NuxtLink to="/contact" class="ma-5">
+        <span class="grey--text">Contact</span>
+      </NuxtLink>
     </v-app-bar>
     <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
+      <Nuxt />
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
+  <v-footer
+    :absolute="!fixed"
+    app
+  >
+    <v-row
+      justify="space-between"
+      no-gutters
     >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+      <strong>Powered By : Techminate</strong>
+      <strong>Copyright@Techminate {{ new Date().getFullYear() }} </strong>
+    </v-row>
+  </v-footer>
+
   </v-app>
 </template>
 
@@ -92,26 +47,13 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
     }
   }
 }
 </script>
+<style>
+  a {
+    text-decoration: none;
+  }
+</style>
--- pages/team/index.vue
+++ pages/team/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    Home
+      team
   </div>
 </template>
 
