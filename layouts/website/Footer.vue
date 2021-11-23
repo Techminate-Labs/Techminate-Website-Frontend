82036ac4d11b5aeb0c86e02a4f2b9582fa5158a8
SazidAhmed <sazidahmed.official@gmail.com>
2021-11-23 23:24:00 +0600

    footer design fixed

--- layouts/website/Footer.vue
+++ layouts/website/Footer.vue
@@ -6,39 +6,28 @@
           <v-row>
               <v-col cols="12" sm="12" md="3" lg="3">
                 <h3 class="mb-2">community</h3>
-                  <span v-for="(item, index) in data" :key="index" class="d-flex my-2 " justify="center" align="center">
-                    <span align="center" justify="center"> 
-                        <v-icon left size="25">{{ item.icon }}</v-icon>
-                    </span>
-                    <span>{{ item.name }}</span>    
-                  </span>
+                <div v-for="(item, index) in data" :key="index">
+                  <v-icon left size="25" class="mt-1">{{ item.icon }}</v-icon>
+                  <span>{{ item.name }}</span>    
+                </div>
               </v-col>
               <v-col cols="12" sm="12" md="3" lg="3">
                 <h3 class="mb-2">Useful Links</h3>
-                  <span v-for="(item, index) in data" :key="index" class="d-flex my-2 " justify="center" align="center">
-                      <span align="center" justify="center"> 
-                          <v-icon left size="25">{{ item.icon }}</v-icon>
-                      </span>
-                      <span>{{ item.name }}</span>    
-                    </span>
+                <div v-for="(item, index) in data" :key="index">
+                  <span>{{ item.name }}</span>    
+                </div>
               </v-col>
               <v-col cols="12" sm="12" md="3" lg="3">
                 <h3 class="mb-2">Resources</h3>
-                  <span v-for="(item, index) in data" :key="index" class="d-flex my-2 " justify="center" align="center">
-                      <span align="center" justify="center"> 
-                          <v-icon left size="25">{{ item.icon }}</v-icon>
-                      </span>
-                      <span>{{ item.name }}</span>    
-                  </span>
+                 <div v-for="(item, index) in data" :key="index">
+                  <span>{{ item.name }}</span>    
+                </div>
               </v-col>
               <v-col cols="12" sm="12" md="3" lg="3">
                 <h3 class="mb-2">Others</h3>
-                   <span v-for="(item, index) in data" :key="index" class="d-flex my-2 " justify="center" align="center">
-                      <span align="center" justify="center"> 
-                          <v-icon left size="25">{{ item.icon }}</v-icon>
-                      </span>
-                      <span>{{ item.name }}</span>    
-                    </span>
+                 <div v-for="(item, index) in data" :key="index">
+                  <span>{{ item.name }}</span>    
+                </div>
               </v-col>
           </v-row>
         </v-container>
