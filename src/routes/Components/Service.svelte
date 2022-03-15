4b7673678f73b17b3349647b05f1cab32ae5a194
afrinsarker <afrinsarker91@gmail.com>
2022-03-15 09:23:34 +0600

    updated responsive design

--- src/routes/Components/Service.svelte
+++ src/routes/Components/Service.svelte
@@ -9,45 +9,49 @@
  <!----card component-->
  <main>
      <h1 class="text-center text-3xl py-6">Our Expertise</h1>
-     <div class="grid grid-cols-1 sm:grid-cols-4 px-5 gap-4">
-        <div class="card w-86 bg-base-100 shadow-xl">
+     <div class="grid grid-cols-1 sm:grid-cols-4 px-5 gap-4 items-center">
+        <div class="card card-side  w-86 bg-base-100 shadow-xl">
              <figure class="px-5 pt-10">
-                 <img src="/Techminate.png" alt="Shoes" class="rounded-xl" />
+                 <img src="https://4.imimg.com/data4/CO/YS/MY-29352968/samsung-desktop-computer-500x500.jpg" alt="Shoes" class="rounded-xl" />
              </figure>
-             <div class="card-body items-center text-center">
+             <div class="card-body mt-10 items-center text-center">
                  <h2 class="card-title">{people[0].name}</h2>
                  <p>{people[0].company}</p>
             </div>
         </div>
-         <div class="card w-86 bg-base-100 shadow-xl">
+         <div class="card card-side w-86 bg-base-100 shadow-xl">
              <figure class="px-5 pt-10">
-                 <img src="/Techminate.png" alt="Shoes" class="rounded-xl" />
+                 <img src="https://4.imimg.com/data4/CO/YS/MY-29352968/samsung-desktop-computer-500x500.jpg" alt="Shoes" class="rounded-xl" />
              </figure>
-             <div class="card-body items-center text-center">
+             <div class="card-body mt-10 items-center text-center">
                  <h2 class="card-title">{people[0].name}</h2>
                  <p>{people[0].company}</p>
              </div>
         </div>
-        <div class="card w-86 bg-base-100 shadow-xl">
+        <div class="card card-side w-86 bg-base-100 shadow-xl">
              <figure class="px-5 pt-10">
-                 <img src="/Techminate.png" alt="Shoes" class="rounded-xl" />
+                 <img src="https://4.imimg.com/data4/CO/YS/MY-29352968/samsung-desktop-computer-500x500.jpg" alt="Shoes" class="rounded-xl" />
              </figure>
-             <div class="card-body items-center text-center">
+             <div class="card-body mt-10 items-center text-center">
                  <h2 class="card-title">{people[0].name}</h2>
                  <p>{people[0].company}</p>
              
              </div>
         </div>
-        <div class="card w-86 bg-base-100 shadow-xl">
+        <div class="card card-side w-86 bg-base-100 shadow-xl">
          <figure class="px-5 pt-10">
-             <img src="/Techminate.png" alt="Shoes" class="rounded-xl" />
+             <img src="https://4.imimg.com/data4/CO/YS/MY-29352968/samsung-desktop-computer-500x500.jpg" alt="Shoes" class="rounded-xl" />
          </figure>
-         <div class="card-body items-center text-center">
+         <div class="card-body mt-10 items-center text-center">
              <h2 class="card-title">{people[0].name}</h2>
              <p>{people[0].company}</p>
          
          </div>
     </div>
      </div>
+
+     
+
+
  </main>
  <!----End card component-->
--- src/routes/Components/footer.svelte
+++ src/routes/Components/footer.svelte
@@ -4,25 +4,24 @@
         <a class=" normal-case text-xl">Techminate</a>
     </div>
     <div>
-      <ul class="footer-title">Company</ul> 
-      <li class="link link-hover">About us</li> 
-      <li class="link link-hover">Contact</li> 
-      <li class="link link-hover">Jobs</li> 
-      <li class="link link-hover">Press kit</li>
+      <span class="footer-title">Services</span> 
+      <a class="link link-hover">Branding</a> 
+      <a class="link link-hover">Design</a> 
+      <a class="link link-hover">Marketing</a> 
+      <a class="link link-hover">Advertisement</a>
     </div> 
     <div>
-      <ul class="footer-title">Company</ul> 
-      <li class="link link-hover">About us</li> 
-      <li class="link link-hover">Contact</li> 
-      <li class="link link-hover">Jobs</li> 
-      <li class="link link-hover">Press kit</li>
+      <span class="footer-title">Company</span> 
+      <a class="link link-hover">About us</a> 
+      <a class="link link-hover">Contact</a> 
+      <a class="link link-hover">Jobs</a> 
+      <a class="link link-hover">Press kit</a>
     </div> 
     <div>
-      <ul class="footer-title">Company</ul> 
-      <li class="link link-hover">About us</li> 
-      <li class="link link-hover">Contact</li> 
-      <li class="link link-hover">Jobs</li> 
-      <li class="link link-hover">Press kit</li>
+      <span class="footer-title">Legal</span> 
+      <a class="link link-hover">Terms of use</a> 
+      <a class="link link-hover">Privacy policy</a> 
+      <a class="link link-hover">Cookie policy</a>
     </div>
   </footer>
   <div class="footer items-center p-4 bg-neutral text-neutral-content">
